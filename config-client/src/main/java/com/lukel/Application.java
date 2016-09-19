69476ac322beb931c8d9c268ad6fd0eb3ab7b0e3
liuls <123>
2016-09-19 09:17:33 +0800

    配置客户端测试项目

---
+++ config-client/src/main/java/com/lukel/Application.java
@@ -0,0 +1,37 @@
+package com.lukel;
+
+/**
+ * Created by admin on 16/9/17.
+ */
+    import org.springframework.beans.factory.annotation.Value;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+    import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+    import org.springframework.boot.context.web.SpringBootServletInitializer;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
+
+    /**
+     * Created by liaokailin on 16/4/28.
+     */
+    @SpringBootApplication
+    @RestController
+    public class Application  extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
+        @Value("${mysql.server.url}")
+        String bar;
+
+        @RequestMapping("/")
+        String hello() {
+            return "Hello " + bar + "!";
+        }
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+
+        @Override
+        public void customize(ConfigurableEmbeddedServletContainer container) {
+            container.setPort(9999);
+        }
+    }
